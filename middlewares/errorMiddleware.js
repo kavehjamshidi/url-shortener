88ca942e550551976d886ff4de5dc0207f94ac9b
kavehjamshidi <kaveh.jamshidi98@hotmail.com>
2021-01-10 17:20:19 +0330

    added 500 status code after logging the error

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -3,8 +3,8 @@ const logger = require('../utils/logger');
 // This module handles errors occurring inside express
 
 module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
   logger.error(err.message, err);
+  err.statusCode = err.statusCode || 500;
   if (err.isPublic) {
     return res
       .status(err.statusCode)
